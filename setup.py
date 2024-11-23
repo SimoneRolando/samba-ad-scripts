4e2eb18ddea3a42687ab332602d047c1fb553e82
Simone Rolando <simone.rolando@ferrarispancaldo.net>
2024-11-23 02:28:48 +0100

    Renamed library files more sensibly

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fp_ad_tools',
+    version='0.1',
+    packages=find_packages(include=['modules', 'modules.*']),
+    install_requires=[]
+)
